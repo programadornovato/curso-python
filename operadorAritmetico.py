db83f527023ac7f04d1b3444158b1eea74cad91b
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-18 15:26:05 -0500

    PYTHON operadores aritméticos[6]🐍👈👈👈
    Video:
    https://youtu.be/n30-8G4sV2s

---
+++ operadorAritmetico.py
@@ -0,0 +1,18 @@
+num1=5
+num2=3
+resultado=num1+num2
+print("La suma es: ",resultado)
+resultado=num1-num2
+print("La resta es: ",resultado)
+resultado=num1*num2
+print("La multiplicacion es: ",resultado)
+resultado=num1/num2
+print("La divicion es: ",resultado)
+resultado=num1//num2
+print("La divicion entera es: ",resultado)
+resultado=num1%num2
+print("El modulo es: ",resultado)
+resultado=num1**num2
+print("El exponente es: ",resultado)
+resultado=3**3 * ( 13/5 * (2*4) )
+print("El resultado es: ",resultado)
