24c82f53c68121b0a4acf6ef1f69638068de3bca
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-17 20:16:03 -0500

    PYTHON comentarios[5]🐍👈👈👈
    Video:
    https://youtu.be/w2srFARplkg

--- variableTiposDeDatos.py
+++ variableTiposDeDatos.py
@@ -1,7 +1,17 @@
+'''
+Una variable es una palabra que puede almacenar algún tipo de dato y python maneja 3 tipos de datos:
+  Número
+  Coma flotante
+  Texto
+  Booleano: Falso o Verdadero
+Además vamos a aprender el tipado dinámico.
+'''
+#Declaramos y imprimimos una variable de tipo numero y su tipo
 numero=10
 print (numero)
 print(type(numero))
 print("-------------")
+#Declaramos y imprimimos una variable y su tipo
 numeroComaFlotante=10.25
 print (numeroComaFlotante)
 print(type(numeroComaFlotante))
