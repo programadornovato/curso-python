b86222ee4851f544268520ded72340741445a165
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-17 19:45:27 -0500

    PYTHON Tipos de Datos [04]🐍
    Video:
    https://youtu.be/1iO_XPM9smw

---
+++ variableTiposDeDatos.py
@@ -0,0 +1,23 @@
+numero=10
+print (numero)
+print(type(numero))
+print("-------------")
+numeroComaFlotante=10.25
+print (numeroComaFlotante)
+print(type(numeroComaFlotante))
+print("-------------")
+texto="hola soy una serpiente 🐍"
+print (texto)
+print(type(texto))
+print("-------------")
+booleano=True
+print (booleano)
+print(type(booleano))
+print("-------------")
+suma=numero+numeroComaFlotante
+print(suma)
+print(type(suma))
+print("-------------")
+suma=texto
+print(suma)
+print(type(suma))
