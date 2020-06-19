c02da3a8bc4d07e34e5d3a3654228f924c4683af
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-19 18:55:16 -0500

    https://youtu.be/XymEuM_8Tig
    Youtube:
    https://youtu.be/XymEuM_8Tig

---
+++ salidaCosola.py
@@ -0,0 +1,5 @@
+nombre="Eugenio"
+edad=30
+#print ("Hola humano tu nombre es",nombre," y tu edad es",edad)
+#print("Hola humano tu nombre es {0} y tu edad es {1:10.3f} adios {0}".format(nombre,edad))
+print(f"Hola humano tu nombre es {nombre} edad {edad:10.3f} adios {nombre}")
