e4def772a4ec8eb2137e6f18e6b571580c150c5f
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-18 19:21:56 -0500

    PYTHON Operadores Relacionales [8]🐍👈👈👈
    Video:
    https://youtu.be/LvruNrg5pbg

---
+++ operadorRelacionales.py
@@ -0,0 +1,17 @@
+num1=10
+num2=20
+num3=30
+resultado=num1<num2
+print(num1 ,"es < que ",num2,":",resultado)
+resultado=num1>num2
+print(num1 ,"es > que ",num2,":",resultado)
+resultado=num1==num2
+print(num1 ,"es == que ",num2,":",resultado)
+resultado=num1<=num2
+print(num1 ,"es <= que ",num2,":",resultado)
+resultado=num1>=num2
+print(num1 ,"es >= que ",num2,":",resultado)
+resultado=num1!=num2
+print(num1 ,"es != que ",num2,":",resultado)
+resultado=num1+num2==num3
+print(num1+num2 ,"es == que ",num3,":",resultado)
