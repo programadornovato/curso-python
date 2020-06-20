f63fb7645b11918d50af6dd0e868d95e20638e17
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-20 16:18:01 -0500

    PYTHON INPUT STRING (Como usar la funcion input en python)[12]🐍👈👈👈
    Video:
    https://youtu.be/QIzNktTeZMA

---
+++ entradaDatos.py
@@ -0,0 +1,6 @@
+nombre=input("Humano ingresa tu nombre: ")
+edad=input("Humano tambien ingresa tu edad: ")
+edad=int(edad)
+print(f"Humano este es tu nombre: {nombre} y tu edad es de {edad} años ")
+dias=edad*365
+print(f"Humano hace {dias:,} dias que naciste")
