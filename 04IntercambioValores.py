e08dd8cd39675965ef65a1cb810f45e6fc64da4c
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-22 14:20:38 -0500

    16.-Curso de PYTHON 2019 [Ejercicio 04.- Intercambio de valores de dos variables]🐍👈👈👈
    Video:
    https://youtu.be/M-dB3hh5P6M

---
+++ 04IntercambioValores.py
@@ -0,0 +1,9 @@
+print("Humano ingresa los pich... valores que se te piden a continuacion")
+a=int(input("El valor de a:"))
+b=int(input("El valor de b:"))
+aux=a
+a=b
+b=aux
+print("Humano aqui estan tus ingaos valores:")
+print(f"Valor a {a}")
+print(f"Valor b {b}")
