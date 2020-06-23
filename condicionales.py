ba729fab5d0628b162c4918791ffba075ee37313
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-23 00:55:27 -0500

    Condicionales if elif y else en Python[19]🐍👈👈👈
    Video:
    https://youtu.be/kQ8VpBnNXLY

---
+++ condicionales.py
@@ -0,0 +1,8 @@
+numero=int(input("Humano por favor ingresa un numero POSITIVO:"))
+if numero<0:
+    print("Humano estupido que parte de numero POSITIVO NO ENTIENDES.")
+elif numero==0:
+    print("Humano estupido el 0 no es positivo, es neutro.")
+else:
+    print("Buen chico ahora dame la patita.")
+    
