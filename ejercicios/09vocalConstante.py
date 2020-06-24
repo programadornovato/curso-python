8b6cb6c8151bfb650c06c7a928375face0fde1e3
Programador Novato <50611379+programadornovato@users.noreply.github.com>
2020-06-24 00:43:48 -0500

    PYTHON Ejercicio 9.- Determinar si es vocal o consonante [24]🐍👈👈👈
    Video:
    https://youtu.be/CZ6P0rgVZv8

---
+++ ejercicios/09vocalConstante.py
@@ -0,0 +1,6 @@
+letra=input("Humano ingresa una constante:").lower()
+#letra=letra.lower()
+if letra=="a" or letra=="e" or letra=="i" or letra=="o" or letra=="u":
+    print("Humano tonto te pedi una constante")
+else:
+    print("Buen chico le atinaste")
