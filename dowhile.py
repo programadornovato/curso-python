27957bcd379d2a3cef0e42aad723a180b40a7c38
Programador Novato <info@programadornovato.com>
2020-07-03 03:42:25 -0500

    PYTHON Ciclo Do While [44]🐍👈👈👈
    Video:
    https://youtu.be/Hi5ZlQcqTO0

---
+++ dowhile.py
@@ -0,0 +1,13 @@
+#Con do while
+while True:
+    letra=input("Humano digital la letra 'a':")
+    if letra!='a':
+        break
+print("Humano tonto te dije que precionaras la letra 'a'")
+'''
+#Con while
+letra=input("Humano digital la letra 'a':")
+while letra=='a':
+    letra=input("Humano digital la letra 'a':")
+print("Humano tonto te dije que precionaras la letra 'a'")
+'''
