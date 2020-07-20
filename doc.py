1d273af4f3f51d335a280d4a0f990675e51dc7bd
Programador Novato <info@programadornovato.com>
2020-07-20 18:09:12 -0500

    PYTHON Documentación con Docstring [85]🐍👈👈👈
    Video:
    https://youtu.be/GjeXCFbT988

---
+++ doc.py
@@ -0,0 +1,13 @@
+def funcionQueHacealgo():
+    '''Esta funcion por ahora no hace nada pero va hacer algo muy importante que va a cambiar el mundo'''
+    pass
+def funcionQueHacealgo2():
+    '''Esta otra funcion por ahora no hace nada pero va hacer algo muy importante que va a cambiar el mundo'''
+    pass
+def funcionQueHacealgo3():
+    '''Esta otra funcion por ahora no hace nada pero va hacer algo muy importante que va a cambiar el mundo'''
+    pass
+'''
+print("NOMBRE DE NUESTRA FUNCION:",funcionQueHacealgo2.__name__)
+print("DOCUMENTACION DE NUESTRA FUNCION:",funcionQueHacealgo2.__doc__)
+'''
