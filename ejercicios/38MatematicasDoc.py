126ba9398f8522add8c18cfd43f653d7cadd0a7c
Programador Novato <info@programadornovato.com>
2020-07-20 20:42:35 -0500

    PYTHON Ejercicio 38.-Documentar nuestro módulo de funciones matemáticas [86]🐍👈👈👈
    Video:
    https://youtu.be/EqHE2MkzxyE

---
+++ ejercicios/38MatematicasDoc.py
@@ -0,0 +1,14 @@
+'''Este modulo realiza operaciones matematicas como sumar, restar, multiplicar y dividir'''
+def suma(a,b):
+    '''Esta funcion suma los parametros a y b'''
+    return a+b
+def resta(a,b):
+    '''Esta funcion resta los parametros a y b'''
+    return a-b
+def mul(a,b):
+    '''Esta funcion multiplicacion los parametros a y b'''
+    return a*b
+def div(a,b):
+    '''Esta funcion divicion los parametros a y b'''
+    return a/b
+pi=3.1416
