022fbdea39289da799cf89c670fc6b320c4d7c9d
Programador Novato <info@programadornovato.com>
2020-07-23 22:29:35 -0500

    PYTHON POO Programación Orientada a Objetos [93]🐍👈👈👈
    Video:
    https://youtu.be/lHdAylIAx4o

---
+++ POO.py
@@ -0,0 +1,17 @@
+class Auto:
+    color="Rojo"
+    modelo="2010"
+    marca="Vocho"
+
+
+vocho1=Auto()
+vocho1.color="Naranja"
+print(vocho1.color,vocho1.modelo,vocho1.marca)
+vocho2=Auto()
+vocho2.color="Verde"
+print(vocho2.color,vocho2.modelo,vocho2.marca)
+beattle=Auto()
+beattle.color="Gris"
+beattle.marca="Beattle"
+beattle.modelo="2015"
+print(beattle.color,beattle.modelo,beattle.marca)
