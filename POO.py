6bacab3fc6f3873475f222d0df3cd91692d43e40
Programador Novato <info@programadornovato.com>
2020-07-24 18:09:25 -0500

    PYTHON Métodos de las Clases  [94]🐍👈👈👈
    Video:
    https://youtu.be/Lu1g97sTEFY

--- POO.py
+++ POO.py
@@ -1,9 +1,48 @@
 class Auto:
+    #Atributos
     color="Rojo"
     modelo="2010"
     marca="Vocho"
+    encendido=False
+    velocidad=0
+    #Metodos
+    def encender(self):
+        self.encendido=True
+        print("El auto esta encendido")
+    def acelera(self):
+        if self.encendido==True:
+            self.velocidad=self.velocidad+10
+    def frena(self):
+        if self.velocidad>0:
+            self.velocidad=self.velocidad-10
+    def apaga(self):
+        if self.encendido==True:
+            self.encendido=False
+            self.velocidad=0
+
+vocho1=Auto()
+vocho1.acelera()
+print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
+vocho1.encender()
+vocho1.acelera()
+vocho1.acelera()
+vocho1.acelera()
+vocho1.acelera()
+vocho1.acelera()
+vocho1.acelera()
+vocho1.acelera()
+vocho1.acelera()
+print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
+vocho1.frena()
+vocho1.frena()
+vocho1.frena()
+print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
+vocho1.apaga()
+print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
 
 
+
+'''            
 vocho1=Auto()
 vocho1.color="Naranja"
 print(vocho1.color,vocho1.modelo,vocho1.marca)
@@ -15,3 +54,5 @@ beattle.color="Gris"
 beattle.marca="Beattle"
 beattle.modelo="2015"
 print(beattle.color,beattle.modelo,beattle.marca)
+'''
+
