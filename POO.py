30bc78e380624b63b31961e950a104ab8ce6d896
Programador Novato <info@programadornovato.com>
2020-07-27 02:22:19 -0500

    PYTHON Métodos y Atributos Públicos y Privados en  [95]🐍👈👈👈
    Video:
    https://youtu.be/mUal8nRKkBg

--- POO.py
+++ POO.py
@@ -1,58 +1,40 @@
 class Auto:
-    #Atributos
+    #Atributos publicos
     color="Rojo"
     modelo="2010"
     marca="Vocho"
     encendido=False
     velocidad=0
-    #Metodos
-    def encender(self):
-        self.encendido=True
-        print("El auto esta encendido")
+    #Atributos privados
+    __llave="123456"
+    #Metodos publicos
+    def encender(self,llave):
+        if self.__llave==llave:
+            self.encendido=True
+            print("El auto esta encendido")
+        else:
+            print("Humano ratero esa no es la llave")
     def acelera(self):
         if self.encendido==True:
             self.velocidad=self.velocidad+10
     def frena(self):
         if self.velocidad>0:
             self.velocidad=self.velocidad-10
+            self.__enciendeLuzFreno()
     def apaga(self):
         if self.encendido==True:
             self.encendido=False
             self.velocidad=0
-
+    #Metodos privados
+    def __enciendeLuzFreno(self):
+        print("Luz del freno encendida")
 vocho1=Auto()
-vocho1.acelera()
-print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
-vocho1.encender()
-vocho1.acelera()
-vocho1.acelera()
+#vocho1.__enciendeLuzFreno()
+#print(vocho1.__llave)
+vocho1.encender("123456")
 vocho1.acelera()
 vocho1.acelera()
 vocho1.acelera()
 vocho1.acelera()
-vocho1.acelera()
-vocho1.acelera()
-print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
-vocho1.frena()
-vocho1.frena()
 vocho1.frena()
 print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
-vocho1.apaga()
-print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
-
-
-
-'''            
-vocho1=Auto()
-vocho1.color="Naranja"
-print(vocho1.color,vocho1.modelo,vocho1.marca)
-vocho2=Auto()
-vocho2.color="Verde"
-print(vocho2.color,vocho2.modelo,vocho2.marca)
-beattle=Auto()
-beattle.color="Gris"
-beattle.marca="Beattle"
-beattle.modelo="2015"
-print(beattle.color,beattle.modelo,beattle.marca)
-'''
-
