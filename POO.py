fe4e8eaa0737f5cd46f0f8447ed00aa74b26925b
Programador Novato <info@programadornovato.com>
2020-07-27 21:50:13 -0500

    PYTHON Variables de Clase y Variables de Instancia [97]🐍👈👈👈
    Video:
    https://youtu.be/uUm5aRJ1pBo

--- POO.py
+++ POO.py
@@ -1,12 +1,12 @@
 class Auto:
     #Atributos publicos
-    color=""
-    modelo=""
-    marca=""
     encendido=False
     velocidad=0
+    _alto=1.5
+    _ancho=2.3
+    _velocidadMaxima=100
+
     #Atributos privados
-    __llave=""
     #Metodos publicos
     def __init__(self,llave,color,modelo,marca):
         self.__llave=llave
@@ -22,7 +22,8 @@ class Auto:
             print("Humano ratero esa no es la llave")
     def acelera(self):
         if self.encendido==True:
-            self.velocidad=self.velocidad+10
+            if self.velocidad<self._velocidadMaxima:
+                self.velocidad=self.velocidad+10
     def frena(self):
         if self.velocidad>0:
             self.velocidad=self.velocidad-10
@@ -34,27 +35,11 @@ class Auto:
     #Metodos privados
     def __enciendeLuzFreno(self):
         print("Luz del freno encendida")
-vocho1=Auto("LL123","Blanco","2010","Vocho")
-vocho1.encender("LL123")
-vocho1.acelera()
-vocho1.acelera()
-vocho1.acelera()
-print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad," Color=",vocho1.color)
 
-vocho2=Auto("LL111","Azul","2009","Vocho")
-vocho2.encender("123")
-vocho2.acelera()
-vocho2.acelera()
-vocho2.acelera()
-print("Encendido=",vocho2.encendido," Velocidad=",vocho2.velocidad," Color=",vocho2.color)
-'''
-#vocho1.__enciendeLuzFreno()
-#print(vocho1.__llave)
-vocho1.encender("123456")
-vocho1.acelera()
-vocho1.acelera()
-vocho1.acelera()
-vocho1.acelera()
-vocho1.frena()
-print("Encendido=",vocho1.encendido," Velocidad=",vocho1.velocidad)
-'''
+Auto._alto=2
+print(Auto._alto)
+print(Auto._ancho)
+print(Auto._velocidadMaxima)
+vocho1=Auto("1","rojo","2010","vocho")
+print(vocho1.color)
+print(vocho1._alto)
