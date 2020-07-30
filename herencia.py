0ee3a4432630b6495ee75382f2db48e4498b9905
Programador Novato <info@programadornovato.com>
2020-07-30 05:01:52 -0500

    PYTHON Herencia Múltiple [102]🐍👈👈👈
    Video:
    https://youtu.be/iWaaBofpeZI

--- herencia.py
+++ herencia.py
@@ -2,13 +2,29 @@ class Persona:
     def __init__(self,nombre,edad):
         self.nombre=nombre
         self.edad=edad
+class Encargado:
+    def __init__(self,codigoEncargado,departamento):
+        self.codigoEncargado=codigoEncargado
+        self.departamento=departamento
+
+class EstudianteEncargado(Persona,Encargado):
+    def __init__(self,nombre,edad,clave,calificacion,codigoEncargado,departamento):
+        Persona.__init__(self,nombre,edad)
+        Encargado.__init__(self,codigoEncargado,departamento)
+        self.clave=clave
+        self.calificacion=calificacion
+        self.codigoEncargado=codigoEncargado
+        self.departamento=departamento
 class Estudiante(Persona):
     def __init__(self,nombre,edad,clave,calificacion):
         Persona.__init__(self,nombre,edad)
         self.clave=clave
         self.calificacion=calificacion
-
+estudianteEncargado1=EstudianteEncargado("Juan",22,"0232",10,"0233","Sistemas")
+print("Nombre",estudianteEncargado1.nombre,"Departamento",estudianteEncargado1.departamento)
+'''
 juan=Persona("Juan",22)
 print("juan.edad",juan.edad)
 maria=Estudiante("Maria",23,"2222",10)
 print("maria clave",maria.clave)
+'''
