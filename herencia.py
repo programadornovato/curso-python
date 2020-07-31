29b9a76b8504a873771af47010e3207089755cf6
Programador Novato <info@programadornovato.com>
2020-07-30 21:19:47 -0500

    PYTHON Sobreescritura de Métodos (override) [103]🐍👈👈👈
    Video:
    https://youtu.be/E1woIHM7ezU

--- herencia.py
+++ herencia.py
@@ -2,6 +2,8 @@ class Persona:
     def __init__(self,nombre,edad):
         self.nombre=nombre
         self.edad=edad
+    def mostrarNombre(self):
+        print("El nombre de la PERSONA es",self.nombre)
 class Encargado:
     def __init__(self,codigoEncargado,departamento):
         self.codigoEncargado=codigoEncargado
@@ -15,13 +17,22 @@ class EstudianteEncargado(Persona,Encargado):
         self.calificacion=calificacion
         self.codigoEncargado=codigoEncargado
         self.departamento=departamento
+    '''
+    def mostrarNombre(self):
+        print("El nombre del ESTUDIANTE-ENCARGADO es",self.nombre)
+    '''
 class Estudiante(Persona):
     def __init__(self,nombre,edad,clave,calificacion):
         Persona.__init__(self,nombre,edad)
         self.clave=clave
         self.calificacion=calificacion
 estudianteEncargado1=EstudianteEncargado("Juan",22,"0232",10,"0233","Sistemas")
-print("Nombre",estudianteEncargado1.nombre,"Departamento",estudianteEncargado1.departamento)
+estudianteEncargado1.mostrarNombre()
+'''
+persona1=Persona("Juan",22)
+persona1.mostrarNombre()
+'''
+#print("Nombre",estudianteEncargado1.nombre,"Departamento",estudianteEncargado1.departamento)
 '''
 juan=Persona("Juan",22)
 print("juan.edad",juan.edad)
